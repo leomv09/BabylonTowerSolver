2c3f0e63be6173a75bdaf945ee150a1071506bd8
yoelapu <yoelapu@gmail.com>
2015-10-05 00:15:47 -0600

    File validation

--- ui/file.py
+++ ui/file.py
@@ -1,5 +1,5 @@
 import json
-
+values = ['*', '-', 'R', 'G', 'B', 'Y']
 
 def save(path, start_matrix, end_matrix):
     """Save a configuration to a file.
@@ -35,7 +35,8 @@ def load(path):
         start = _parse_matrix(obj["start"])
         end = _parse_matrix(obj["end"])
 
-        return start, end
+        errors = validate_matrix(start, "start") + validate_matrix(end, "end")
+        return start, end, errors
 
 
 def _parse_matrix(matrix):
@@ -44,3 +45,100 @@ def _parse_matrix(matrix):
 
 def _parse_row(row):
     return [str(x) for x in row]
+
+
+def validate_matrix(matrix, name):
+    """Validate  matrix
+
+    parameters:
+        Matrix matrix -- the matriz to validate
+
+
+    return:
+        [String] return matrix  errors
+    """
+    errors = "######  Matrix   " + name + ' errors    ######\n '
+    errors += validate_values(matrix)
+    errors += validate_structure(matrix)
+    errors += validate_amount_balls(matrix)
+    return errors
+
+
+def validate_values(matrix):
+    """Validate values of a matrix
+
+    parameters:
+        Matrix matrix -- the matriz to validate
+        String name -- ID name for the matrix
+
+    return:
+        [String] matrix values errors
+    """
+    errors = ""
+    row_in = 1
+    for row in matrix:
+            for value in row:
+                if (value in values)==False:
+                    error = "Caracter invalido  "+"'" + str(value) + "'" + "    Fila # " + str(row_in) + "\n"
+                    errors += error
+                row_in += 1
+    return errors
+
+
+def validate_structure(matrix):
+    """Validate  matrix structure
+
+    parameters:
+        Matrix matrix -- the matriz to validate
+
+
+    return:
+        [String] matrix structure errors
+    """
+    errors = ""
+    if len(matrix)!=5:
+        errors += "Error en la cantidad de filas"
+    row_in = 1
+    for row in matrix:
+        if len(row)!=4:
+            errors += "Error en la cantidad de columnas" + " Fila  #" + str(row_in)+ "\n"
+        row_in += 1
+    return errors
+
+
+def validate_amount_balls(matrix):
+    """Validate  amount of balls
+
+    parameters:
+        Matrix matrix
+
+
+    return:
+        [String] amount balls errors
+    """
+    balls = [0, 0, 0, 0, 0, 0] #[red balls, Green balls , Blue balls,Yellow balls,-,*]
+    errors = ""
+    for row in matrix:
+            balls[0] += row.count("R")
+            balls[1] += row.count("G")
+            balls[2] += row.count("B")
+            balls[3] += row.count("Y")
+            balls[4] += row.count("-")
+            balls[5] += row.count("*")
+
+    if balls[0] != 4:
+        errors += "error en la cantidad de esferas rojas aparecen " + str(balls[0]) + "\n"
+    if balls[1] != 4:
+        errors += "error en la cantidad de esferas verdes aparecen " + str(balls[1]) + "\n"
+    if balls[2] != 4:
+        errors += "error en la cantidad de esferas azules aparecen " + str(balls[2]) + "\n"
+    if balls[3] != 4:
+        errors += "error en la cantidad de esferas amarillas aparecen " + str(balls[3]) + "\n"
+    if balls[4] != 3:
+        errors += "error en la cantidad de - aparecen " + str(balls[4]) + "\n"
+    if balls[5] != 1:
+        errors += "error en la cantidad de * aparecen " + str(balls[5]) + "\n"
+    return errors
+
+
+load("test.txt")
